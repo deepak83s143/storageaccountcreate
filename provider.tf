498efd8263d50ff385d1dbbaae1559b23a90220d
Deepak Sharma <deepak83s143@gmail.com>
2025-09-28 23:44:17 +0530

    add code

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.12.2"
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "4.32.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "mademi-rg"
+    storage_account_name = "mademistg1"
+    container_name = "mademistatefiles"
+    key = "stg.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "3805d418-505b-4d26-9254-6523d1b472cb"
+}
