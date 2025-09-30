d35f147d9c546462b17ffad871605a11bed1dbad
Deepak Sharma <71786253+deepak83s143@users.noreply.github.com>
2025-09-30 12:29:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,21 +5,3 @@ resource "azurerm_storage_account" "k8sstg" {
   account_tier = "Standard"
   account_replication_type = "LRS"
 }
-
-
-resource "azurerm_storage_share" "k8sshare" {
-  depends_on = [ azurerm_storage_account.k8sstg ]
-  name = "k8sshare"
-  quota = 5
-  storage_account_id = azurerm_storage_account.k8sstg.id
-}
-
-data "azurerm_storage_account" "k8sstg1" {
-  name = azurerm_storage_account.k8sstg.name
-  resource_group_name = azurerm_storage_account.k8sstg.resource_group_name
-}
-
-output "k8sstg-key" {
-  value = data.azurerm_storage_account.k8sstg1.primary_access_key
-  sensitive = true
-}
